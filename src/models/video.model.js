04fdab0946d21bf77613b8fb8b413fa38c054cae
srikar <srikar.code01@gmail.com>
2025-01-16 22:31:54 +0530

    added video model

---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        trim: true,
+        lowercase: true,
+        index: true,
+        unique: true
+    },
+    videoFile: {
+        type: String, // string from cloudinary url
+        required: true
+    },
+    thumbnail: {
+        type: String, // string from cloudinary url
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+
+    // when a video is uploaded on platforms like cloudinary they inreturn gives us the information of the video like the url of the video etc.. in the same way we also get the information about duration of the video
+
+    duration: {
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    }
+}, {timestamps: true});
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
