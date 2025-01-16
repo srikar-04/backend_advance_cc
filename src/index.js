a58d4f6ddd8c3bcf69bfec74e2cb061d257221e6
srikar <srikar.code01@gmail.com>
2025-01-16 18:56:26 +0530

    app.js configuration

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
 
+    app.on("error", (error) => {
+        console.log('ERROR : ', error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB Connection Failed!!!", error); 
+})
 
 
 
