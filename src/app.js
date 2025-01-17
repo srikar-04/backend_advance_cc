ee21fcb90b9eb7cf47f27907c0b3b61c7571db05
srikar <srikar.code01@gmail.com>
2025-01-17 20:28:44 +0530

    added user route using middleware

--- src/app.js
+++ src/app.js
@@ -15,4 +15,17 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// routes importing
+
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+// WE SHOULD USE A MIDDLEWARE TO USE THE ROUTES
+
+app.use('/api/v1/users', userRouter) // when the user types /users then the control reaches to the userRouter file and all the routes of the user are handled there
+
+// this will be the route
+// http://localhost:8000/api/v1/users/register
+
 export { app }
