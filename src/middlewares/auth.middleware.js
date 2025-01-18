4606e8e73bf1dbcd1e02b4d708d0490fbcffa12a
srikar <srikar.code01@gmail.com>
2025-01-18 17:02:47 +0530

    wrapped in try-catch block

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,22 +4,26 @@ import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js"
 
 export const verifyJWT = asyncHandler(async(req, res,next) => {
-    const token = req.cookies?.acessToken ||req.header("Authorization")?.replace("Bearer ", "")
-
-    if(!token) {
-        throw new ApiError(401, "Unauthorized request")
+    try {
+        const token = req.cookies?.acessToken ||req.header("Authorization")?.replace("Bearer ", "")
+    
+        if(!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).
+        select("-password -refreshToken")
+    
+        if(!user) {
+            throw new ApiError(401, "invalid acess token")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid acess token")
     }
-
-    const decodedToken = jwt.verify(token, process.env.ACESS_TOKEN_SECRET)
-
-    const user = await User.findById(decodedToken?._id).
-    select("-password -refreshToken")
-
-    if(!user) {
-        throw new ApiError(401, "invalid acess token")
-    }
-
-    req.user = user;
-    next()
 
 })
