1ae308283b6fef4f309b8fba8cb670481352e651
srikar <srikar.code01@gmail.com>
2025-01-21 20:31:52 +0530

    chore: add explanation comments to asyncHandler for better understanding of its functionality

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,9 @@
+// EXPLANATION
+// asyncHandler is a higherOrder function(it takes function as an argument and also return a function)
+// the requestHandler function is an asynchronous function which returns a promise
+// the asyncHandler returns a "middleware function" which has acess to req, res, next objects
+// the promise returned by the requestHandler is resolved in this asyncHandler, without writing try-catch syntax for all the await tasks in the requestHandler
+
 const asyncHandler = (requestHandler) => {
     return (req, res, next)  => {
         Promise
