091b661ed1dfd49c813a73e5f5d0462e98447b9d
srikar-04 <srikar.code01@gmail.com>
2025-08-29 10:12:51 +0530

    compiled ts files

---
+++ backend-ts/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World");
+let a = 10;
+let b = 20;
+let c = a + b;
+//# sourceMappingURL=index.js.map
--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -1,1 +1,4 @@
 console.log("Hello World");
+let a: number = 10;
+let b: number = 20;
+let c: number = a + b;
