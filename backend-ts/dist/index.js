b9b8fd29a6dfa0c62b89e72aa060fe125914ccba
srikar-04 <srikar.code01@gmail.com>
2025-08-29 11:13:32 +0530

    added gitignore file

--- backend-ts/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello World");
-let a = 10;
-let b = 20;
-let c = a + b;
-//# sourceMappingURL=index.js.map
--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -2,3 +2,5 @@ console.log("Hello World");
 let a: number = 10;
 let b: number = 20;
 let c: number = a + b;
+
+let d: number = 30;
