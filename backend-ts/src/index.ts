1eb46dceb7c3c2c1de430c0da4e34cdfe1629735
srikar-04 <srikar.code01@gmail.com>
2025-08-29 11:55:02 +0530

    setting up node-ts project with best practices

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -1,4 +1,8 @@
+import dotenv from "dotenv"
+dotenv.config()
 import express, { type Request, type Response } from "express";
+
+const PORT = process.env.PORT || 5000
 
 const app = express();
 
@@ -6,6 +10,6 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello World from new ts project");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
 });
