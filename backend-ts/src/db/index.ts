afea170ec06def1647b08e8ca908990d3bd59b88
srikar-04 <srikar.code01@gmail.com>
2025-08-30 22:23:27 +0530

    code for mongodb connection

---
+++ backend-ts/src/db/index.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import mongoose from "mongoose";
+
+export default async function connectDB() {
+    try {
+        const MONGODB_URI = process.env.MONGODB_URI
+        const MONGODB_NAME = process.env.MONGODB_NAME
+    
+        if (!MONGODB_NAME || !MONGODB_URI) throw new Error('missing mongo db connection string or mongodb name')
+    
+        const connectionInstance = await mongoose.connect(`${MONGODB_URI}/${MONGODB_NAME}`)
+        console.log('connectionInstance', connectionInstance)
+        console.log('Data base connected successfully!!', connectionInstance.connection.host)
+
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Initial data base connection failed!! : ', error.message)
+        } else {
+            console.error('Initial data base connection failed!! : ', error)
+        }
+        process.exit(1)
+    }
+}
+
+connectDB()
+
+mongoose.connection.on('error', (err) => {
+    console.error('Database post connection error : ', err)
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.warn('Database disconnected!!')
+})
--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -1,8 +1,31 @@
-import dotenv from "dotenv"
-dotenv.config()
-import app from "#app.js";
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app.js";
+import connectDB from "./db/index.js";
+import { exit } from "process";
 
 const PORT = process.env.PORT || 3000
 
-// code for connecting database
+// code for connecting database and "server level error handling"
 
+connectDB()
+.then(() => {
+    const server = app.listen(PORT)
+
+    server.on('connect', () => {
+        console.log(`app is listening on port ${PORT}`);
+    })
+
+    server.on('error', () => {
+        console.error('server error occured');
+    })
+
+    server.on('close', () => {
+        console.warn('server closed !!');
+        
+    })
+})
+.catch((err) => {
+    console.error('mongodb connection failed : ', err)
+    exit(1)
+})
