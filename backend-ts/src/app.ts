5d83a441b50a7855470b147b74bf6731122aa36a
srikar-04 <srikar.code01@gmail.com>
2025-08-30 22:36:23 +0530

    installed and added cors and cookie-parser to express

--- backend-ts/src/app.ts
+++ backend-ts/src/app.ts
@@ -1,8 +1,16 @@
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
 import express, { type Request, type Response, type Express } from "express";
-
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(cookieParser())
 
 export default app
