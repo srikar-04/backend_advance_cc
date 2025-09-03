53737e31150f0eaf4dc292b702a583ee5b36b07f
srikar-04 <srikar.code01@gmail.com>
2025-09-03 10:03:57 +0530

    writing an asyncHandler function for handling asynchronous requests

---
+++ backend-ts/src/utils/asyncHandler.ts
@@ -0,0 +1,21 @@
+import { type Request, type Response, type NextFunction, type RequestHandler } from "express";
+
+type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>
+
+// return type of the parent function is a RequestHandler because the
+// function that it is returning is an express middleware function
+// all express middleware functions are of the type RequestHandler
+const asyncHandler = (asyncHandler: AsyncHandler): RequestHandler => {
+
+    // returning a function inside a function
+    // this is an express middleware function
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise
+        .resolve(asyncHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+
+}
+
+
+export { asyncHandler };
