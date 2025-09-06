204eb8f980ea0e1e34da1295a2506e60c4610198
srikar-04 <srikar.code01@gmail.com>
2025-09-06 22:41:24 +0530

    installed zod and added schema for validating user in the api level

---
+++ backend-ts/src/schemas/user.schema.ts
@@ -0,0 +1,16 @@
+import * as z from 'zod'
+
+const user = z.object({
+    username: z.string().trim().toLowerCase().min(3, {error: 'username should be above 3 characters'}),
+    email: z.email({
+        error: 'invalid email format'
+    }),
+    password: z.string().trim().min(6, {error: 'password should be above 6 characters'}),
+    refreshToken: z.jwt({error: 'invalid jwt found'}).optional(),
+    fullName: z.string().trim().min(3, {error: 'fullname should be above 3 characters'}),
+    watchHistory: z.array(
+        z.string().regex(/^[a-fA-F0-9]{24}$/, {error: "Invalid ObjectId Format"})
+    ),
+    avatar: z.string().optional(),
+    coverImage: z.string().optional()
+})
